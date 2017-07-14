7a9199e115f6dda443e0ab7afe5d210ab8d5b105
kingweiang <kingweiang@163.com>
2017-07-14 16:04:48 +0800

    login countdown

--- Common/Account/Account.js
+++ Common/Account/Account.js
@@ -11,35 +11,66 @@ import {
     Text,
     View
 } from 'react-native';
+var {CountDownText} = require('react-native-sk-countdown');
+
 
 var Account=React.createClass({
-    render() {
+    render: function(){
         return (
             <View style={styles.container}>
-                <Text style={styles.welcome}>
-                    Welcome to Account!
-                </Text>
+                <Text style={styles.tip}>{'CountDown in seconds \n 以秒为单位的倒计时'}</Text>
+                <View style={styles.row}>
+                    <CountDownText
+                        style={styles.cd}
+                        countType='seconds' // 计时类型：seconds / date
+                        auto={true} // 自动开始
+                        afterEnd={() => {}} // 结束回调
+                        timeLeft={10} // 正向计时 时间起点为0秒
+                        step={-1} // 计时步长，以秒为单位，正数则为正计时，负数为倒计时
+                        startText='获取验证码' // 开始的文本
+                        endText='获取验证码' // 结束的文本
+                        intervalText={(sec) => sec + '秒重新获取'} // 定时的文本回调
+                    />
+                </View>
+                <Text style={styles.tip}>{'CountDown in timestamp \n 以日期-时间为单位的倒计时'}</Text>
+                <View style={styles.row}>
+                    <CountDownText // 倒计时
+                        style={styles.cd}
+                        countType='date' // 计时类型：seconds / date
+                        auto={true} // 自动开始
+                        afterEnd={() => {}} // 结束回调
+                        timeLeft={10} // 正向计时 时间起点为0秒
+                        step={-1} // 计时步长，以秒为单位，正数则为正计时，负数为倒计时
+                        startText='' // 开始的文本
+                        endText='' // 结束的文本
+                        intervalText={(date, hour, min, sec) => date + '天' + hour + '时' + min + '分' + sec} // 定时的文本回调
+                    />
+                </View>
             </View>
-        );
+        )
     }
 });
+
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
+        justifyContent: 'space-around',
         alignItems: 'center',
         backgroundColor: '#F5FCFF',
     },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
+    row: {
+        padding: 7,
+        backgroundColor: 'red',
+        borderRadius: 7,
     },
-    instructions: {
+    tip: {
+        fontSize: 20,
+    },
+    cd: {
         textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
+        color: 'white',
+        fontSize: 20,
     },
 });
 
--- Common/Account/login.js
+++ Common/Account/login.js
@@ -19,6 +19,7 @@ import {
 import Button from 'react-native-button';
 var request = require('../../Common/Main/request')
 var config = require('../../Common/Main/config')
+var {CountDownText} = require('react-native-sk-countdown')
 
 var Login=React.createClass({
     getInitialState(){
@@ -26,6 +27,7 @@ var Login=React.createClass({
             phoneNumber:'',
             codeSent:false,
             verifyCode:'',
+            countingDone:false,
         }
     },
 
@@ -34,7 +36,11 @@ var Login=React.createClass({
             codeSent:true
         })
     },
-
+    _countingDone(){
+        this.setState({
+            countingDone:true
+        })
+    },
     _sendVerifyCode(){
         var that = this
         var phoneNumber = this.state.phoneNumber
@@ -44,8 +50,8 @@ var Login=React.createClass({
         var body={
             phoneNumber:phoneNumber
         }
-        var url = config.header.api.base+config.header.api.signup
-        request.post(url,body)
+        var Surl = config.header.api.base+config.header.api.signup
+        request.post(Surl,body)
             .then((data)=>{
                 if(data &&data.success){
                     that._showVerifyCode()
@@ -56,6 +62,36 @@ var Login=React.createClass({
             .catch((err)=>{
                 Platform.OS=='ios'? AlertIOS.alert('获取验证码失败，请检查手机网络是否正常！'):Alert.alert('获取验证码失败，请检查手机网络是否正常！')
             })
+    },
+    _submit(){
+        var that = this
+        var phoneNumber = this.state.phoneNumber
+        var verifyCode = this.state.verifyCode
+
+        if(!phoneNumber || !verifyCode){
+            return  Platform.OS=='ios'? AlertIOS.alert('手机号或验证码不能为空！'):Alert.alert('手机号或验证码不能为空！')
+        }
+
+        var body={
+            phoneNumber:phoneNumber,
+            verifyCode:verifyCode
+        }
+
+        var Vurl = config.header.api.base+config.header.api.verify
+
+        request.post(Vurl,body)
+            .then((data)=>{
+                if(data &&data.success){
+                    console.log('login OK')
+                    console.log(data)
+                }else{
+                    Platform.OS=='ios'? AlertIOS.alert('获取验证码失败，请检查手机号码是否正确！'):Alert.alert('获取验证码失败，请检查手机号码是否正确！')
+                }
+            })
+            .catch((err)=>{
+                Platform.OS=='ios'? AlertIOS.alert('获取验证码失败，请检查手机网络是否正常！'):Alert.alert('获取验证码失败，请检查手机网络是否正常！')
+            })
+
     },
     render() {
         return (
@@ -84,7 +120,7 @@ var Login=React.createClass({
                                 placeholder="请输入验证码"   // 站位符
                                 autoCaptialize={'none'}     // 不处理大小写
                                 autoCorrect={false}          // 不纠正对错
-                                keyboradType={'number-pad'}  // 键盘类型  数字型
+                                keyboardType ={'number-pad'}  // 键盘类型  数字型
                                 style={styles.inputField}
                                 underlineColorAndroid="transparent"
                                 onChangeText={(text)=>{
@@ -93,6 +129,24 @@ var Login=React.createClass({
                                     })
                                 }}
                             />
+                            {
+                                this.state.countingDone
+                                    ?<Button
+                                    stlye={styles.countBtn}
+                                    onPress={this._sendVerifyCode()}
+                                ><Text style={{marginTop:10,color:'blue'}}>重新获取验证码</Text></Button>
+                                    :<CountDownText
+                                    style={styles.countBtn}
+                                    countType='seconds' // 计时类型：seconds / date
+                                    auto={true} // 自动开始
+                                    afterEnd={this._countingDone} // 结束回调
+                                    timeLeft={5} // 正向计时 时间起点为0秒
+                                    step={-1} // 计时步长，以秒为单位，正数则为正计时，负数为倒计时
+                                    startText='获取验证码' // 开始的文本
+                                    endText='获取验证码' // 结束的文本
+                                    intervalText={(sec) =>'剩余'+ sec +'秒' } // 定时的文本回调
+                                />
+                            }
                         </View>:null
                     }
                     {
@@ -130,13 +184,13 @@ const styles = StyleSheet.create({
     inputField:{
         // flex:1,
         // padding:15,
+        width:160,
         height:40,
         color:'#666',
         fontSize:16,
         backgroundColor:'#fff',
         borderRadius:4,
-        borderWidth:1,
-        marginTop:5,
+        // borderWidth:1,
 
     },
     btn:{
@@ -149,6 +203,24 @@ const styles = StyleSheet.create({
         color:'#ee735c',
         fontSize:Platform.OS=='ios'?16:14,
         fontWeight:Platform.OS=='ios'?'400':'200',
+    },
+    verifyCodeBox:{
+        marginTop:10,
+        flexDirection:'row',
+        justifyContent:'space-between',
+    },
+    countBtn:{
+        width:80,
+        height:40,
+        padding:10,
+        marginLeft:8,
+        color:'white',
+        backgroundColor:'#ee735c',
+        borderColor:'#ee735c',
+        textAlign:'left',
+        fontWeight:'600',
+        fontSize:15,
+        borderRadius:2,
     }
 });
 
